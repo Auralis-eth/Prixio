e15284aef5844faf6706844370488c9550ab93bf
pharms.eth <100330083+pharms-eth@users.noreply.github.com>
2025-09-22 09:43:02 -0600

    Initial Commit

---
+++ PrixioTests/PrixioTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrixioTests.swift
+//  PrixioTests
+//
+//  Created by Daniel Bell on 9/22/25.
+//
+
+import Testing
+@testable import Prixio
+
+struct PrixioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
